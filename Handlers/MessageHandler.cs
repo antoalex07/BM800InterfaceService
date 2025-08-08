7b3ec24b086a70f9fbad2131dedd584202b7e462
antoalex07 <antoalex710@gmail.com>
2025-08-08 11:49:16 +0530

    removed unused headers

--- Handlers/MessageHandler.cs
+++ Handlers/MessageHandler.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//using System.Text;
+﻿using System.Text;
 using System.Xml;
-using Microsoft.Extensions.Logging;
 
 namespace TestService.Handlers
 {
