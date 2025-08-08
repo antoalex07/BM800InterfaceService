5486ed542453160036332a2fe704c80da1b1f4fa
antoalex07 <antoalex710@gmail.com>
2025-08-08 11:52:29 +0530

    Communication method router

---
+++ Services/CommunicationFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace TestService.Services
+{
+    public class CommunicationFactory
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ConfigurationService _configService;
+
+        public CommunicationFactory(ILoggerFactory loggerFactory, ConfigurationService configService)
+        {
+            _loggerFactory = loggerFactory;
+            _configService = configService;
+        }
+
+        public INetworkService CreateCommunicationService()
+        {
+            var config = _configService.GetConfiguration();
+
+            return config.CommunicationType.ToUpper() switch
+            {
+                "TCP" => new NetworkService(_loggerFactory.CreateLogger<NetworkService>(), _configService),
+                "SERIAL" => new SerialService(_loggerFactory.CreateLogger<SerialService>(), _configService),
+                _ => throw new InvalidOperationException($"Unsupported communication type: {config.CommunicationType}")
+            };
+        }
+    }
+}
